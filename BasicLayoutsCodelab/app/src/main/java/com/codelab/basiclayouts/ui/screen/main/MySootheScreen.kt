88a5f58ad0f9f0e2f0949500a3e405c51a719653
AhmetSIRIM <ahmet.sirim@outlook.com>
2024-09-16 14:04:34 +0300

    Complete 'Home screen - Scrolling' step

--- BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/ui/screen/main/MySootheScreen.kt
+++ BasicLayoutsCodelab/app/src/main/java/com/codelab/basiclayouts/ui/screen/main/MySootheScreen.kt
@@ -1,14 +1,38 @@
 package com.codelab.basiclayouts.ui.screen.main
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.codelab.basiclayouts.R
+import com.codelab.basiclayouts.ui.screen.main.component.AlignYourBodyRowComponent
+import com.codelab.basiclayouts.ui.screen.main.component.FavoriteCollectionsGridComponent
+import com.codelab.basiclayouts.ui.screen.main.component.MySootheSectionComponent
+import com.codelab.basiclayouts.ui.screen.main.component.SearchBarComponent
 import com.codelab.basiclayouts.ui.theme.MySootheTheme
 
 @Composable
 fun MySootheScreen(modifier: Modifier = Modifier) {
-    Text("TODO(\"MySootheScreen not yet implemented\")")
+    Column(
+        modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+        Spacer(Modifier.height(16.dp))
+
+        SearchBarComponent(Modifier.padding(horizontal = 16.dp))
+
+        MySootheSectionComponent(title = R.string.align_your_body) { AlignYourBodyRowComponent() }
+
+        MySootheSectionComponent(title = R.string.favorite_collections) { FavoriteCollectionsGridComponent() }
+
+        Spacer(Modifier.height(16.dp))
+    }
 }
 
 @Preview(showBackground = true)
